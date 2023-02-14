d8ccdf61d3e24e7ab336b7713352aab225047880
Eli Ennab <elinaennab@gmail.com>
2023-02-14 21:38:27 +0100

    Forgot this deleted file with earlier album services name.

--- src/services/album_service.ts
+++
@@ -1,8 +0,0 @@
-import prisma from '../prisma'
-import { CreateAlbumData } from '../types'
-
-export const createAlbum = async (data: CreateAlbumData) => {
-	return await prisma.album.create({
-		data
-	})
-}
