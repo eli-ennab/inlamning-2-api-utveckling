ddd6455308edc95802f13d221ed4b0ab88373b85
Eli Ennab <elinaennab@gmail.com>
2023-02-20 18:05:40 +0100

    Validate to see that photo_id is not empty and is an array.

--- src/routes/albums.ts
+++ src/routes/albums.ts
@@ -4,7 +4,7 @@
 import express from 'express'
 import { index, show, store, update, deleteAlbum, addPhotosToAlbum, removePhotoFromAlbum } from '../controllers/album_controller'
 const router = express.Router()
-import { createAlbumRules, updateAlbumRules } from '../validations/album_rules'
+import { addPhotosToAlbumRules, createAlbumRules, updateAlbumRules } from '../validations/album_rules'
 
 /**
  * GET /albums
@@ -34,7 +34,7 @@ router.patch('/:albumId', createAlbumRules, update)
  * POST /albums/:albumId/photos
  * Add multiple photos to an album
  */
-router.post('/:albumId/photos', [], addPhotosToAlbum)
+router.post('/:albumId/photos', addPhotosToAlbumRules, addPhotosToAlbum)
 
 /**
  * DELETE /albums/:albumId/photos/:photoId
--- src/validations/album_rules.ts
+++ src/validations/album_rules.ts
@@ -10,3 +10,11 @@ export const createAlbumRules = [
 export const updateAlbumRules = [
 	body('title').isString().withMessage('has to be a string').isLength({ min: 3 }).withMessage('has to at least 3 chars long'),
 ]
+
+export const addPhotosToAlbumRules = [
+	body('photo_id').notEmpty().withMessage('has to contain at least one int').isArray().withMessage('has to be an array'),
+]
+
+export const removePhotoFromAlbumRules = [
+
+]
